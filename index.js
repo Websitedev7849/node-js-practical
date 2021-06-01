9b0b585d8225c14c867a579c051405e40241afc5
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-06-01 20:43:37 +0530

    started with question 4

---
+++ index.js
@@ -0,0 +1,6 @@
+//Create a Node.js Application that accepts first name, last name and date of birth of a Person and define a Module that concatenate firstName and lastName and also calculate the age of the Person.
+const readline = require('readline');
+
+const rl = readline({
+    input: 
+});
