c52a7ce1049094949159ca56fcaa65cceb34e978
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-06-01 20:53:01 +0530

    parsed birthdate, age to be calculated

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 //Create a Node.js Application that accepts first name, last name and date of birth of a Person and define a Module that concatenate firstName and lastName and also calculate the age of the Person.
 const readline = require('readline');
 
-const rl = readline({
-    input: 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
+
+function form() {
+
+   
+    
+    rl.question("please enter a first name : " , firstName => {
+
+        rl.question("please enter a last name : " , lastName => {
+
+            rl.question("please enter a date : " , dob => {
+
+                dob = dob.split('/');
+                let birthDay = parseInt(dob[0]);
+                let birthMonth = parseInt(dob[1]);
+                let birthYear = parseInt(dob[2]);
+
+                console.log(birthDay, birthMonth, birthYear);
+            
+            });
+        
+        });
+        
+    });
+
+    
+}
+
+form();
