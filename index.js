118877fcc1db65f1266c6fd5c5c66b82affaa04f
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-06-02 19:37:05 +0530

    question 6 solved

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 // Create a Node.js Application to count occurrence of given word in a file and display the count on console.
 
 const readline = require('readline');
+const fs = require('fs');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question("Please enter kW : ", kW => {
-    console.log(`You entered ${kW}`);
-});
+function findWord() {
+    fs.readFile('./paragraph.txt', {encoding: 'utf-8'}, (err,data)=>{
+        rl.question("Please enter a word to search : ", word => {
+            data = data.split(" ");
+            let count = 0;
+            for (let i = 0; i < data.length; i++) {
+                if (word == data[i]) {
+                    count++;
+                }            
+            }
+            console.log(`${count} matches found for word "${word}"`);
+            findWord();
+        });    
+    });
+}
+findWord();
