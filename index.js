c3e0927e58a431cbf6b200269f39330ed8f58a1c
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-06-02 20:51:22 +0530

    question 11 solved

---
+++ index.js
@@ -0,0 +1,5 @@
+//Create Node is application using user defined Rectangle module to find area of rectangle and display the details on console.
+const l = process.argv[2];
+const b = process.argv[3];
+
+console.log(`the area of rectangle is ${l*b}`);
